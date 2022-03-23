2b25f0d61c0db27da8c3b84c05fe866f28923442
Daniil <koversundanil@gmail.com>
2022-03-23 22:33:29 +0200

    Added FAQ page

--- profile.php
+++ profile.php
@@ -19,7 +19,7 @@ include "core.php";
     <span class="spotlight"></span>
 </div>
 <iframe style="position:absolute; top: 10px" width="350" height="200" src="https://www.youtube.com/embed/VpSe5v6HORs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<a href="/" style="position: absolute; top: 500px;"><button class="btn btn-danger">Выйти Из Зоны Комфорта</button></a>
+<a href="/admin.php" style="position: absolute; top: 500px;"><button class="btn btn-danger">Выйти Из Зоны Комфорта</button></a>
 
 <script src="application/assets/js/profile.js"></script>
 </body>
